08377040b6d5ec04cd1e5cc5845f50f203b8f13f
alexpreciado94 <alexpreciado94@hotmail.com>
2021-10-19 11:41:40 +0200

    Commit 3

--- procesos.php
+++ procesos.php
@@ -19,14 +19,20 @@
       for($i=0;$i<$resultado->num_rows;$i++){
         $fila = mysqli_fetch_assoc($resultado);
         echo '<tr>
+          <td>'.$fila['id'].'</td>
           <td>'.$fila['dni'].'</td>
           <td>'.$fila['nombre'].'</td>
+          <td>'.$fila['email'].'</td>
+          <td>'.$fila['telefono'].'</td>
+          <td><a href="modificar.html?'.$fila[i].'">Modificar</a></td>
+          <td><a href="borrar.html?'.$fila[i].'">Modificar</a></td>
         </tr>';
       }
       echo '</table>';
     }
     function añadir(){
-
+      $sql = 'select * from empleados';
+      $resultado = $this->conexion->consultar($sql);
     }
     function modificar(){
 

da7a2a2db1579c2d266a2da5d2d0b83e5f2a9110
alexpreciado94 <alexpreciado94@hotmail.com>
2021-10-19 13:33:19 +0200

    Commit 5

--- borrar.php
+++ borrar.php
@@ -3,7 +3,22 @@
     function __construct(){
       include_once 'procesos.php';
       $this->procesoBorrar = new Procesos();
-      $this->procesoborrar->borrar();
+      $this->iniciar();
+    }
+    function iniciar(){
+      if(!isset($_get['enviar'])){
+        echo '<form action="#" method="get">
+          <select name="aceptar">
+            <option value="0">NO</option>
+            <option value="1">SI</option>
+          </select>
+          <input type="submit" name="enviar" value="ENVIAR" />
+        </form>';
+      }else{
+        if($_get['aceptar']==1){
+          $this->procesoborrar->borrar();
+        }
+      }
     }
   }
 ?>
--- procesos.php
+++ procesos.php
@@ -31,19 +31,22 @@
       echo '</table>';
     }
     function anadir($formularioAnadir){
-      $sql = 'insert into empleados values ('.$formularioAnadir['newId'].', '
-      .$formularioAnadir['newDNI'].', '.$formularioAnadir['newNombre'].', '
-      .$formularioAnadir['newEmail'].', '.$formularioAnadir['newTelefono'].');';
+      $sql = "insert into empleados values (".$formularioAnadir['newId'].", '"
+      .$formularioAnadir['newDNI'].'", "'.$formularioAnadir['newNombre'].'", "'
+      .$formularioAnadir['newEmail'].'", "'.$formularioAnadir['newTelefono']."');";
       $resultado = $this->conexion->consultar($sql);
     }
-    function modificar(){
-      $sql = 'select * from empleados';
+    function modificar($formularioModificar){
+      $sql = "update empleados set dni='"
+      .$formularioModificar['modificarDNI'].'", nombre="'.$formularioModificar['modificarNombre'].'", email="'
+      .$formularioModificar['modificarEmail'].'", telefono="'.$formularioModificar['modificarTelefono']."'
+      where id=".$formularioModificar['modificarId'].";";
       $resultado = $this->conexion->consultar($sql);
 
       header('Location:index.php');
     }
-    function borrar(){
-      $sql = 'select * from empleados';
+    function borrar($borrarId){
+      $sql = "delete * from empleados where id=".$borrarId.";";
       $resultado = $this->conexion->consultar($sql);
 
       header('Location:index.php');

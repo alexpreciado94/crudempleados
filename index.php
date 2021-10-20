91479346fe195b9cb414b409e6cbec5ee5527968
alexpreciado94 <alexpreciado94@hotmail.com>
2021-10-20 14:12:27 +0200

    CSS

--- index.php
+++ index.php
@@ -3,12 +3,55 @@
   <head>
     <meta charset="utf-8">
     <title>Empleados PHP</title>
+    <link rel="stylesheet" href="empleados.css">
   </head>
   <body>
-    <?php
-      include_once 'procesos.php';
-      $procesos = new Procesos();
-      $procesos->listar();
-    ?>
+    <header>
+			<a href="index.html"><img src="images/logo.png"></a><nav>
+				<ul>
+					<li><a href="index.html">INICIO</a></li>
+					<li><a href="https://www.php.net/manual/es/intro-whatis.php">PHP</a></li>
+					<li><a href="recibido.php">RESPUESTAS</a></li>
+					<li><a href="about.html">NOSOTROS</a></li>
+				</ul>
+			</nav>
+		</header>
+    <main>
+      <aside>
+        <nav>
+          <ul>
+            <li><a href="">BOTON</a></li>
+            <li><a href="">BOTON</a></li>
+            <li><a href="">BOTON</a></li>
+            <li><a href="">BOTON</a></li>
+            <li><a href="">BOTON</a></li>
+            <li><a href="">BOTON</a></li>
+            <li><a href="">BOTON</a></li>
+            <li><a href="">BOTON</a></li>
+            <li><a href="">BOTON</a></li>
+            <li><a href="">BOTON</a></li>
+            <li><a href="">BOTON</a></li>
+            <li><a href="">BOTON</a></li>
+            <li><a href="">BOTON</a></li>
+            <li><a href="">BOTON</a></li>
+            <li><a href="">BOTON</a></li>
+            <li><a href="">BOTON</a></li>
+            <li><a href="">BOTON</a></li>
+            <li><a href="">BOTON</a></li>
+            <li><a href="">BOTON</a></li>
+          </ul>
+        </nav>
+      </aside>
+      <section>
+        <?php
+          include_once 'procesos.php';
+          $procesos = new Procesos();
+          $procesos->listar();
+        ?>
+      </section>
+    </main>
+    <footer>
+
+    </footer>
   </body>
 </html>
